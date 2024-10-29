ff8db08c0ef894b4a64875b43d0290a727b5e48a
hithesh svsk <62789585+SVSKHD@users.noreply.github.com>
2024-10-29 23:10:21 +0530

    existing positions

---
+++ efficient/tradeCondition.py
@@ -0,0 +1,11 @@
+import MetaTrader5 as mt5
+import asyncio
+
+
+async def check_the_opposite_direction_with_respect_to_thresholds(threshold):
+    if threshold<=0.5:
+        print("hello threshold")
+
+
+
+async def check_existing_positions():
